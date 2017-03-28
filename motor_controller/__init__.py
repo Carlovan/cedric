92d97348e54ba1e261c91ee2db89fa8a718ed401
Carlovan <giulio.carlassare@gmail.com>
2017-03-28 21:14:04 +0200

    Added RPC and motorcontroller, not sure if it works

---
+++ motor_controller/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from motor import Motor
+import zerorpc
+
+class MotorController:
+	def __init__(self, pin_dx, pin_sx):
+		self.mdx = Motor(pin_dx, False)
+		self.msx = Motor(pin_sx, True)
+
+	def go(self, speed, steer):
+		if -100 <= speed <= 100 and -100 <= steer <= 100:
+			msx_speed = speed + steer
+			mdx_speed = speed - steer
+			tmp1 = max(msx_speed, mdx_speed)
+			tmp2 = min(msx_speed, mdx_speed)
+			if tmp1 > 100:
+				offset = tmp1 - 100
+				msx_speed -= offset
+				mdx_speed -= offset
+			if tmp2 < -100:
+				offset = tmp2 + 100
+				msx_speed += offset
+				mdx_speed += offset
+
+server = zerorpc.Server(MotorController(4, 5))
+server.bind('tcp://127.0.0.1:22000')
+server.run()
