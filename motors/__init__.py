429a9ed21ccaddf1cd5a001f896d6fedc5b43f71
Carlovan <giulio.carlassare@gmail.com>
2017-06-07 14:59:23 +0000

    Now the package is runnable. Values are between -1 and 1 (for consistency)

--- motors/__init__.py
+++ motors/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from .motor import Motor
-import zerorpc
 
 class MotorController:
 	def __init__(self, pin_dx, pin_sx):
@@ -9,6 +8,8 @@ class MotorController:
 		self.msx = Motor(pin_sx, False, 4)
 
 	def walk(self, speed, steer):
+		speed *= 100
+		steer *= 100
 		if -100 <= speed <= 100 and -100 <= steer <= 100:
 			msx_speed = speed + steer
 			mdx_speed = speed - steer
@@ -24,8 +25,4 @@ class MotorController:
 				mdx_speed += offset
 			self.mdx.set_speed(mdx_speed)
 			self.msx.set_speed(msx_speed)
-
-if __name__ == '__main__':
-	server = zerorpc.Server(MotorController(4, 5))
-	server.bind('tcp://127.0.0.1:22000')
-	server.run()
+		return 'ok'
---
+++ motors/__main__.py
@@ -0,0 +1,15 @@
+import zerorpc
+from motors import MotorController
+import RPi.GPIO as GPIO
+
+pin1 = 3
+pin2 = 4
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(pin1, GPIO.OUT)
+GPIO.setup(pin2, GPIO.OUT)
+
+server = zerorpc.Server(MotorController(pin1, pin2))
+server.bind('tcp://127.0.0.1:22000')
+server.run()
