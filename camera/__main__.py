d3db53379c102a2666965940223cbcf0ee486e43
Carlovan <giulio.carlassare@gmail.com>
2017-06-17 15:38:32 +0000

    Now the image is saved to file

--- camera/__main__.py
+++ camera/__main__.py
@@ -1,11 +1,15 @@
 import zerorpc
 import camera
+import time
 
 camera.init()
 
 class CameraController():
 	def shoot(self):
-		return camera.shoot().tolist()
+		filename = '/tmp/image.dat'
+		with open(filename, 'wb') as f:
+			f.write(bytearray(camera.shoot().flatten().tolist()))
+		return filename
 
 server = zerorpc.Server(CameraController(), pool_size=2)
 server.bind('ipc:///tmp/22001')
