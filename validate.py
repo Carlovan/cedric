03f5e9b0f8e44dac2f79e5de92e4a84d8a33b2e0
Carlovan <giulio.carlassare@gmail.com>
2017-07-05 20:52:25 +0000

    Better visualization

--- validate.py
+++ validate.py
@@ -6,5 +6,9 @@ nn = NeuralNetwork([10000, 100, 7])
 
 nn.weights = list(np.load('weights.npy'))
 
+results = []
 for val in data.validation:
-	print(val[0], list(nn.feed(val[1]).A.flatten()))
+	results.append([val[0], list(nn.feed(val[1]).A.flatten())])
+
+results.sort(key=lambda x: x[0])
+print('\n'.join('{}: {}'.format(x[0], x[1]) for x in results))
